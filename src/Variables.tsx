a9cf6eb849cdab981d91d76c8d7ab47d41ba96dc
jetie000 <makeenko.005@gmail.com>
2023-10-14 22:23:14 +0300

    fix: change API url

--- src/Variables.tsx
+++ src/Variables.tsx
@@ -2,7 +2,7 @@ export const variables={
     // API_URL: "https://jetie000-001-site1.ctempurl.com/api",
     // PHOTO_URL: "https://jetie000-001-site1.ctempurl.com/Photos/",
     // SOCKET_URL : "wss://jetie000-001-site1.ctempurl.com/chat-hub",
-    API_URL: "http://jeytikon-001-site1.etempurl.com/api",
+    API_URL: "https://jeytikon-001-site1.etempurl.com/api",
     $LOCAL_USER: "user_local",
     UTC3_MINSK : 3600 * 3 * 1000
 }
